3c1d327e7115eebc37fb814f8645bbb65a52d1ba
shakilahmad72 <iamshakilahmad27@gmial.com>
2026-02-07 19:03:17 +0530

    Initial Commit

--- MinimumElementChallenge/src/dev/lpa/Main.java
+++ MinimumElementChallenge/src/dev/lpa/Main.java
@@ -10,8 +10,15 @@ public class Main {
         int[] returnedArray = readIntegers();
         System.out.println(Arrays.toString(returnedArray));
 
-        int returnedMin = findMin(returnedArray);
-        System.out.println("min = " + returnedMin);
+//        int returnedMin = findMin(returnedArray);
+//        System.out.println("min = " + returnedMin);
+
+//        reverse(returnedArray);
+//        System.out.println("Final: " + Arrays.toString(returnedArray));
+
+        int[] reversedCopy = reverseCopy(returnedArray);
+        System.out.println("After reverse " + Arrays.toString(returnedArray));
+        System.out.println("reversedCopy " + Arrays.toString(reversedCopy));
     }
 
     private static int[] readIntegers() {
@@ -40,5 +47,29 @@ public class Main {
         }
 
         return min;
+    }
+
+    private static void reverse(int[] array) {
+
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
+
+        for (int i = 0; i < halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
+            System.out.println("--> " + Arrays.toString(array));
+        }
+    }
+
+    private static int[] reverseCopy(int[] array) {
+
+        int[] reversedArray = new int[array.length];
+        int maxIndex = array.length - 1;
+        for (int el : array) {
+            reversedArray[maxIndex--] = el;
+        }
+
+        return reversedArray;
     }
 }
