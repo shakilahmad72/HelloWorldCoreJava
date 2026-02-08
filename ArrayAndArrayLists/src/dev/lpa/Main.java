5b299908be6262f8d75775afd6b673f9036982c4
shakilahmad72 <iamshakilahmad27@gmial.com>
2026-02-08 19:01:03 +0530

    Initial Commit

---
+++ ArrayAndArrayLists/src/dev/lpa/Main.java
@@ -0,0 +1,26 @@
+package dev.lpa;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    static void main() {
+
+        String[] originalArray = new String[] {"First", "Second", "Third"};
+        var originalList = Arrays.asList(originalArray);
+
+        originalList.set(0, "one");
+        System.out.println("list: " + originalList);
+        System.out.println("array: " + Arrays.toString(originalArray));
+
+        originalList.sort(Comparator.naturalOrder());
+        System.out.println("array: " + Arrays.toString(originalArray));
+
+//        originalList.add("fourth");
+
+        List<String> newList = Arrays.asList("Sunday", "Monday", "Tuesday");
+        System.out.println(newList);
+    }
+}
