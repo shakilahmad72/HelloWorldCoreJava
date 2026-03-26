d7e05b89a5775b76b90d8559ccc106010317f3c2
shakilahmad72 <iamshakilahmad27@gmial.com>
2026-03-26 10:34:50 +0530

    Initial Commit

--- StreamingStudents/src/dev/lpa/Main.java
+++ StreamingStudents/src/dev/lpa/Main.java
@@ -1,8 +1,10 @@
 package dev.lpa;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -77,12 +79,21 @@ public class Main {
                         .count();
         System.out.println("longTerm students? " + longTermCount);
 
-        Arrays.stream(students)
+        var longTimeLearners = Arrays.stream(students)
                 .filter(s -> (s.getAge() - s.getAgeEnrolled() >= 7) &&
                         (s.getMonthsSinceActive() < 12))
                 .filter(s -> !s.hasProgrammingExperience())
                 .limit(5)
-                .forEach(System.out::println);
+                .toArray(Student[]::new);
+
+        var learners = Arrays.stream(students)
+                .filter(s -> (s.getAge() - s.getAgeEnrolled() >= 7) &&
+                        (s.getMonthsSinceActive() < 12))
+                .filter(s -> !s.hasProgrammingExperience())
+                .limit(5)
+                .collect(Collectors.toList());
+
+        Collections.shuffle(learners);
 
     }
 }
